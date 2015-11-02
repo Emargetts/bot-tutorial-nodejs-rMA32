55c3131d48c4a2f7ed5e68379c2a6fd23cc041d4
Emargetts <erikmargetts@gmail.com>
2015-11-02 16:25:42 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,7 @@ function respond() {
       botRegexbg = /^\/bg/;  botRegexDL = /^\/dl/;botRegexDP = /^\/dp/;botRegexErik = /^\/erip/;
       botRegexHunter=/^\/hunter/;  botRegexkahn = /^\/kahn/;  botRegexSC = /^\/SDL/i; botWilly = /^\/willy/; botzez = /^\/zez/;
       botRegexgf = /^\/gf/;  botRegexfrenk = /^\/frenk/; botRegexSb = /^\/sub/; botRegexhose = /^\/hose.jpg/; botRegexWk = /^\/users/; botRegexcee = /^\/cb/;
+      bothelp = /^\/help/;
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
                 ,"BAL","SD","DEN","MIN","ATL","KC","NYG","GB","DET","HOU","STL","CHI","CAR",
                 "MIA","BUF","SF","WAS","NYJ","TB"]
@@ -87,6 +88,11 @@ function respond() {
   else if(request.text && botRegexcee.test(request.text)) {
     this.res.writeHead(200);
     postMessage("http://i.imgur.com/6PmT6Bj.jpg");
+    this.res.end();
+  }  
+  else if(request.text && bothelp.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("No");
     this.res.end();
   
   
