b4a80aa0212a519ae57d4b7ae4e4aae46775b2e9
Emargetts <erikmargetts@gmail.com>
2016-04-04 17:08:36 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -98,7 +98,7 @@ function respond() {
   }  
   else if(request.text && botStreams.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("(https://www.twitch.tv/crazyknightss,https://www.twitch.tv/hardfluff,https://www.twitch.tv/skarmotastic)");
+    postMessage("(https://www.twitch.tv/crazyknightss [Erik],  https://www.twitch.tv/hardfluff [Liam],  https://www.twitch.tv/skarmotastic  [OKC],  http://www.twitch.tv/steventwerkel [Steve])");
     this.res.end();
   }  
   else if(request.text && botham.test(request.text)) {
